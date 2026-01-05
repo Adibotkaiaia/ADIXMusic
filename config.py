b0249ca458c77843a6706fde6332026957f6a537
Saksham gupta <163002102+Adibotkaiaia@users.noreply.github.com>
2026-01-05 11:58:53 +0530

    Set default SUPPORT_CHAT URL in config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VIP_SUPPORT_II")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", None)
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/VIP_SUPPORT_II")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
